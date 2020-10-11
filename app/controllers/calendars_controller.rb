8fbb62cafe0bd57a20aada5494f83cf4ea9a2d6b
ksk-o <69468089+ksk-o@users.noreply.github.com>
2020-10-11 17:33:55 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,12 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans} #issue1 キーをシンボル記法へ
+      wday_num = Date.today.wday #添字となる数値を得る
+      #もしもwday_numが7以上であれば、7を引く
+      if wday_num >= 7 #条件式を記述
+        wday_num = wday_num - 7
+      end
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num + x]} #wdaysから値を取り出し、日付に合わせて曜日を
       @week_days.push(days)
     end
 
